37b8786515993a5facb2fe2d8fcf4eea2cd10807
Noor Awad <noor16@meet.mit.edu>
2015-02-22 20:02:37 +0200

    player

---
+++ player.py
@@ -0,0 +1,22 @@
+
+import turtle
+class player(turtle):
+	def _init_(self,x,y,health,mana_power,weapon,shape)
+		self.goto(x,y)
+		self.health=health
+		self.mana=mana
+		self.attack=attack
+		self.shape=shape
+
+	def mana_power
+		if mana_power>mana_cost use_mana
+		else print"not enof mana"
+
+	
+	def weapon(self)
+		if press key weapon.show
+		else wepon.hide
+
+	def health
+		if (x,y) of attack=(x,y) of player reduce health by 10
+		
