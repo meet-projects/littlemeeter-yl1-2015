6e69eae91711f966bf02c4bb32ff812766458d79
Noor Awad <noor16@meet.mit.edu>
2015-03-01 20:00:49 +0200

    work update

--- player.py
+++ player.py
@@ -1,22 +1,67 @@
 
 import turtle
 class player(turtle):
-	def _init_(self,x,y,health,mana_power,weapon,shape)
+	def _init_(self,canvas,x,y,dx,dy,health,mana,weapon,shape):
+		RawTurtle.__init__(self,canvas)
+		self.penup()
 		self.goto(x,y)
 		self.health=health
 		self.mana=mana
-		self.attack=attack
-		self.shape=shape
+		self.weapon=weapon
+		self.dx = dx
+        self.dy = dy
+		self.shape("player")
 
-	def mana_power
+
+
+	def use_mana(self):
 		if mana_power>mana_cost use_mana
 		else print"not enof mana"
 
 	
-	def weapon(self)
-		if press key weapon.show
-		else wepon.hide
+	def weapon(self):
+		if  weapon.show():#show inside weapon should return the status of showing and hiding
+			weapon.hide()
 
-	def health
-		if (x,y) of attack=(x,y) of player reduce health by 10
+		else:
+			weapon.show()
+
+	def gethit (self):
+		self.health-10
+
+	def moveup (self):
+		x = self.xcor() 
+        y = self.ycor()
+		 
+		x= x
+		y= self.dy+y
+
+		self.goto(x,y)
+
+	def movedown (self):
+		x = self.xcor() 
+        y = self.ycor()
+		 
+		x= x
+		y= self.dy-y
+
+		self.goto(x,y)
+
+	def moveright (self):
+		x = self.xcor() 
+        y = self.ycor()
 		
+		x= x+self.dx
+		y= y
+		self.goto(x,y)
+
+	def moveleft (self):
+		x = self.xcor() 
+        y = self.ycor()
+		
+		x= x-self.dx
+		y= y
+		self.goto(x,y)
+
+
+turtle.mainloop()
--- weapon.py
+++ weapon.py
@@ -1,24 +1,45 @@
 import turtle
 
 class weapon(Turtle):
-	def _init_(self,x,y,shape,damage,mana_cost):
-		self.penup()		
-		self.hide()
+	def _init_(self,canvas,x,y,dz,dy,shape,damage,mana_cost):
+		RawTurtle.__init__(self,canvas)
+		self.penup()
 		self.goto(x,y)
-		self.shape=shape
 	`	self.damage=damage
 		self.mana_cost=mana_cost
-		mana_use
+		self.dx = dx
+        self.dy = dy
+		self.shape("weapon")
 
+	#def checkifenemyisinrange(self,enemy):
+		#x= self.xcor()
+		#y=self.ycor()
+		#if((self.ycor()=enemy.ycor()) and (enemy.xcor()-self.xcor()<0)):
+					#return True
+		#else:
+			#return False 
+
+		
 	def mana_use(self):
-		def use_mana():
-			mana-self.mana_cost
-		if mana>=self.mana_cost use_mana
-			else print ("not enough mana")
-	def shoot(self,x,y):
-		self.goto(x+screenMaxx,y)
 
-	def damage(self):
+		mana_power-self.mana_cost
+		
+	def shoot(self,distance):
+		x= self.xcor()
+		y=self.ycor()
+                                
+		self.goto(x+distance,y)
+
+
+	def weapon.show(self):
+		turtle.showturtle()
+
+
+	def weapon.hide(self):
+		turtle.hideturtle()
+
+
+
 	
 
 turtle.mainloop()
